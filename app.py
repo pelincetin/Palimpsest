8dae042e5ceaf5b676df1de8dd35d14d460fe185
pelincetin <pc2807@barnard.edu>
2020-12-01 16:08:08 -0500

    my uploads functionality

--- app.py
+++ app.py
@@ -26,48 +26,59 @@ okta_client = UsersClient("https://dev-9288180.okta.com", "004rl1v6L-2Pd0ewr5DcU
 current_id = 6
 pictures = [{
 		"Id": 1,
-		"Name": "Celeste Layne",
+		"Name": "Pelin Cetin",
 		"Caption": "Damage of the Flood",
 		"Date": "2020-11-29",
 		"Poster": "static/uploads/first.jpeg",
 		"Description": "Over the course of the past number of years, flooding along the river and creeks that flow through Livingston Manor has inflicted serious property damage upon the many residences unfortunate enough to be in the path of the river valley's designated flood-plane. Still, the devastation caused over the course of the past few years by these floodwaters pales in comparison to the destruction amassed in just a few short hours by the work of large machines designed to destroy.",
-		"Location": "Livingston Manor"
+		"Location": "Livingston Manor",
+		"Verified": True,
+		"VerificationDate": "2020-11-30",
+
 	},
 	{
 		"Id": 2,
-		"Name": "Stacy Tao",
+		"Name": "Will Cao",
 		"Caption": "Heavy Rainfall",
-		"Date": "2020-11-29",
+		"Date": "2020-11-09",
 		"Poster": "static/uploads/second.jpg",
 		"Description": "Heavy rainfall during the evening of September 30th and the early morning hours of October 1st, estimated by some to total six inches of rain, drenched Livingston Manor and its surroundings, quickly filling the previously water-starved local streams and creeks with floodwaters. Considering the experiences from the recent flooding events at the Manor, this episode would probably be considered minor; unless, of course you are one of the residents still remaining in the flood prone areas.",
-		"Location": "Livingston Manor"
+		"Location": "Livingston Manor",
+		"Verified": True,
+		"VerificationDate": "2020-11-14",
 	},
 	{
 		"Id": 3,
-		"Name": "Celeste Layne",
+		"Name": "Aya Abdallah",
 		"Caption": "The Drastic Change",
 		"Date": "2020-11-29",
 		"Poster": "static/uploads/third.jpeg",
 		"Description": "Livingston Manor has long had a flare for the dramatic, the usual episodes being water related. Yesterday afternoon, however, added a new chapter in Manor misery; fire. In what sounded like the repeated percussion of cannons, the propane tanks alongside the Hoos building exploded, sending shock waves that broke windows and could be felt throughout the downtown section of the village as far away as Peck's Market.",
-		"Location": "Livingston Manor"
+		"Location": "Livingston Manor",
+		"Verified": True,
+		"VerificationDate": "2020-11-14"
 	},
 	{
 		"Id": 4,
-		"Name": "Stacy Tao",
+		"Name": "Amy Huang",
 		"Caption": "Beaverkill Covered Bridge",
-		"Date": "2020-11-29",
+		"Date": "2020-11-19",
 		"Poster": "static/uploads/fourth.jpeg",
 		"Description": "While working on dismantling the approaches to the Beaverkill Covered Bridge during the winter of 2016-17, the bridge restoration work crew unearthed a cache of hides buried within the earthen ramp leading to the bridge’s eastern portal. These hides assumedly date back to the nineteenth century tannery of Wm. Ellswoth & Co. that was located at what is now the Beaverkill Campsite, next to the bridge.",
-		"Location": "Beaverkill Covered Bridge"
+		"Location": "Beaverkill Covered Bridge",
+		"Verified": True,
+		"VerificationDate": "2020-11-20"
 	},
 	{
 		"Id": 5,
-		"Name": "Celeste Layne",
+		"Name": "Pelin Cetin",
 		"Caption": "The Arnold House",
-		"Date": "2020-11-29",
+		"Date": "2020-11-04",
 		"Poster": "https://images.squarespace-cdn.com/content/v1/534d8565e4b0c7f5e4fcb4f7/1562030061043-ICZGG34BMCXHR40HHXGX/ke17ZwdGBToddI8pDm48kFyD7pzB8zoMIVY5aiUuFlp7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0jG2lbcDYBOeMi4OFSYem8DMb5PTLoEDdB05UqhYu-xbnSznFxIRsaAU-3g5IaylIg/foster-supply-hospitality-ARNOLD-2019-06-Conservatory-Food-Scott-Yoga-Exteriors-Lawrence-Braun-0001-LB2_2700.jpg",
 		"Description": "The Arnold House is a lively Catskills getaway located on Shandelee Mountain, near the quaint town of Livingston Manor. With our Tavern, newly renovated Barn & Greenhouse, expansive grounds & hiking trails and access to the area's storied outdoor activities our mission is to treat our guests to the comfort and relaxation of casual country living.",
-		"Location": "Arnold House"
+		"Location": "Arnold House",
+		"Verified": False,
+		"VerificationDate": "2020-11-24"
 	},
 ]
 
@@ -91,11 +102,25 @@ def about():
 
 @app.route('/featured')
 def featured():
-	return render_template('featured.html', pictures=pictures)
+	featured_pics = []
+	for pic in pictures:
+		if pic["Verified"]:
+			featured_pics.append(pic)
+
+	return render_template('featured.html', pictures=featured_pics)
 
 @app.route('/upload')
 def upload_form():
 	return render_template('upload.html')
+
+@app.route('/my-uploads')
+def my_uploads():
+	my_photos=[]
+	name = g.user.profile.firstName + " " + g.user.profile.lastName
+	for pic in pictures:
+		if pic["Name"] == name:
+			my_photos.append(pic)
+	return render_template('my-uploads.html', pictures=my_photos)
 
 @app.route('/uploaded', methods=['GET', 'POST'])
 def upload_image():
